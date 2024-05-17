82c429b0efc16841aacb82d55ea9301417fe93b3
reotsugua <renanaugusto.ras@gmail.com>
2024-05-16 23:26:13 -0300

    conhecendo objetos

---
+++ Javascript/Basico/Entendendo Objetos/main.js
@@ -0,0 +1,12 @@
+const pessoa = {
+    nome: 'Renan',
+    idade: 32,
+
+    descrever: (data) => console.log(`Meu nome é ${data.nome} e 
+    minha idade é ${data.idade}`)
+}
+
+
+
+
+pessoa.descrever(pessoa);
