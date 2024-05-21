644c5123b8b3f9639763f09de61f114b8c1a1591
reotsugua <renanaugusto.ras@gmail.com>
2024-05-20 21:24:48 -0300

    iniciando pokedex

--- Javascript/Basico/Entendendo Objetos/main.js
+++ Javascript/Basico/Entendendo Objetos/main.js
@@ -10,3 +10,27 @@ const pessoa = {
 
 
 pessoa.descrever(pessoa);
+
+const pessoa1 = {
+    nome: 'Renan',
+    anoNascimento: 1992
+};
+
+// Desestrutura o ano de nascimento do objeto pessoa
+const { anoNascimento } = pessoa1;
+
+// Calcula a idade usando o ano atual
+const anoAtual = new Date().getFullYear();
+const idade = anoAtual - anoNascimento;
+
+console.log(`A idade de ${pessoa.nome} é ${idade} anos.`);
+
+const usuario = {
+    nome: 'Carlos',
+    email: 'carlos@example.com'
+};
+
+// Suponha que não sabemos se o usuário forneceu sua idade
+const { nome, email, idade1 = 30 } = usuario;
+console.log(`Nome: ${nome}, Email: ${email}, Idade: ${idade}`);
+console.log(usuario);
