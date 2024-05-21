0cf88200e1113bc4173db009f2cabc2c8da02d15
reotsugua <renanaugusto.ras@gmail.com>
2024-05-21 00:02:15 -0300

    lista feita

---
+++ Javascript/Pokedex/assets/js/main.js
@@ -0,0 +1,58 @@
+async function api() {
+    const response = await fetch('https://pokeapi.co/api/v2/pokemon');
+    return await response.json();
+}
+
+
+async function getPokemon() {
+    const pokemons = await api();
+    const promises = pokemons.results.map(async function (x) {
+        const response = await fetch(x.url);
+        const data = await response.json();
+        return {'name': x.name, 'data': data};
+    } )
+    return await Promise.all(promises);
+}
+
+
+async function insertData(idElement) {
+    const tipos = {
+        grass : 'success',
+        fire : 'danger',
+        water : 'primary',
+        bug : 'dark',
+        normal : 'light'
+    }
+    const teste = document.querySelector(idElement)
+    const data = await getPokemon()
+    const card = data.map((x)=> {
+        const novo = x.data.sprites.other.showdown.front_default;
+        console.log(novo)
+        return `
+        <li class="bg-secondary col-12 col-md-3 col-sm-5 d-flex flex-column list-group-item rounded text-white">
+                <div class=" d-flex justify-content-between">
+                    <span>${x.name}</span>
+                    <span>#${x.data.id}</span>
+                </div>
+                <div class="d-flex justify-content-between my-auto">
+                    <ol class="p-0 d-flex gap-1 flex-column my-auto" style="list-style: none;">
+                        <li class="badge rounded-pill text-bg-${tipos[x.data.types[0].type.name]}">${x.data.types[0].type.name}</li>
+                    </ol>
+                    <img class="align-self-end" style="max-width: 45%;" src="${x.data.sprites.other.showdown.front_default}" alt="" srcset="">
+                </div>
+        </li>
+    ` });
+    teste.innerHTML = card.join('');
+
+    
+    
+}
+
+
+
+// Main
+document.addEventListener('DOMContentLoaded', async function () {
+    const id = '#pokemon-list'
+    insertData(id)
+    
+})
