fc3d3aa6f4b93f1d2db1682bf896f5289baf32f5
Renan Santos <renan.santos@grupoeuro17.com.br>
2025-01-28 15:16:40 -0300

    estudos

--- Typescript/dist/javascript/nova-trasacao-component.js
+++ Typescript/dist/javascript/nova-trasacao-component.js
@@ -1,9 +1,4 @@
 "use strict";
-let saldo = 3000;
-const elementoSaldo = document.querySelector(".saldo-valor .valor");
-if (elementoSaldo != null) {
-    elementoSaldo.textContent = saldo.toString();
-}
 const elementoFormulario = document.querySelector(".block-nova-transacao form");
 elementoFormulario.addEventListener("submit", function (event) {
     event.preventDefault();
@@ -17,17 +12,17 @@ elementoFormulario.addEventListener("submit", function (event) {
     let tipoTransacao = inputTipoTransacao.value;
     let valor = inputValor.valueAsNumber;
     let data = new Date(inputData.value);
-    if (tipoTransacao == "Depósito") {
+    if (tipoTransacao == TipoTransacao.DEPOSITO) {
         saldo += valor;
     }
-    else if (tipoTransacao == "Transferência" || tipoTransacao == "Pagamento de Boleto") {
+    else if (tipoTransacao == TipoTransacao.TRANSFERENCIA || tipoTransacao == TipoTransacao.PAGAMENTO_BOLETO) {
         saldo -= valor;
     }
     else {
         alert("Tipo de Transação é inválido!");
         return;
     }
-    elementoSaldo.textContent = saldo.toString();
+    elementoSaldo.textContent = saldo.toLocaleString('pt-br', { currency: "BRL", style: 'currency' });
     const novaTransacao = {
         tipoTransacao: tipoTransacao,
         valor: valor,
--- Typescript/dist/javascript/typescript.js
+++
@@ -1,2 +0,0 @@
-"use strict";
-let valor = 3000;
